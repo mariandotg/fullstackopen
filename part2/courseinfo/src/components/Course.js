97084f48bddd6f87c3f381cd85b9101ec2e1d5d3
mariandotg <marianguillaume.m@gmail.com>
2022-08-22 18:24:27 -0300

    Added Course component

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,30 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+
+const Course = ({ course }) => {
+    return (
+      <div>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+      </div>
+    );
+  };
+
+  export default Course;
