7e4e6fea5f82bc02cc57c5895f14ff6d1f117387
mariandotg <marianguillaume.m@gmail.com>
2022-08-23 19:40:43 -0300

    Exercise 2.6 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,30 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    const person = { name: newName }
+    event.preventDefault()
+    setPersons((prev) => [...prev, person])
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => <p key={person.name}>{person.name}</p>)}
     </div>
   )
 }
