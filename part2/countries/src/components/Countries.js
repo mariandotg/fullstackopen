53a8a699a612f27757ec3c2bf867b45e221e2242
mariandotg <marianguillaume.m@gmail.com>
2022-08-25 16:17:43 -0300

    Exercise 2.13 completed

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import Country from "./Country"
 
 const Countries = ({ countriesToShow }) => {
+  const [country, setCountry] = useState("");
+
   const len = countriesToShow.length;
+
   return (
     <div>
       {len === 1 ? <Country country={countriesToShow[0]} /> : (
         len <= 10 ? (
           countriesToShow.map((country) => (
-            <div key={country.name.official}>{country.name.common}</div>
+            <div key={country.name.official}>{country.name.common}<button onClick={() => setCountry(country)}>show</button></div>
           ))
         ) : <div>Too many matches, specify another filter</div>
       )}
+      {country ? <Country country={country} /> : null}
     </div>
   )
 }
