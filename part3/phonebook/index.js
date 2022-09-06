9ecfc7f1c16f722f7614f840228a2febd764dab5
mariandotg <marianguillaume.m@gmail.com>
2022-09-06 13:30:00 -0300

    Exercise 3.4 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -50,6 +50,13 @@ app.get("/api/persons/:id", (request, response) => {
   else response.status(404).end()
 })
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
