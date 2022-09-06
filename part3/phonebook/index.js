431f1d9db2f41cb977d0c8d8caa1d02902f21478
mariandotg <marianguillaume.m@gmail.com>
2022-09-06 15:40:39 -0300

    Exercise 3.7 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 
 const PORT = 3001
 
 app.use(express.json())
+app.use(morgan("tiny"))
 
 let persons = [
   {
