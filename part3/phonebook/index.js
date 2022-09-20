2c08f678db20a3fc978267f914c528603a5b5170
mariandotg <marianguillaume.m@gmail.com>
2022-09-20 10:24:46 -0300

    Exercise 3.13 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,12 @@
+require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+const Person = require("./models/person")
 
 const app = express()
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.use(express.json())
 app.use(cors())
@@ -18,34 +20,13 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 )
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-]
-
 app.get("/api/persons", (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 })
 
-app.get("/info", (request, response) => {
+/* app.get("/info", (request, response) => {
   const count = persons.length
   const date = new Date()
 
@@ -92,7 +73,7 @@ app.post("/api/persons", (request, response) => {
   persons = [...persons, newPerson]
 
   response.json(newPerson)
-})
+})*/ 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
