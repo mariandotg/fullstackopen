8a760750e6e2caac42d27307813f5cab9723bfdf
mariandotg <marianguillaume.m@gmail.com>
2022-11-11 15:06:58 -0300

    Exercise 4.4 completed

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.forEach((blog) => {
+    sum += blog.likes
+  })
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
