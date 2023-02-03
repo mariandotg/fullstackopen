d00da727026704568b737d305796ce335e082796
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-02-03 13:06:18 -0300

    Exercise 6.8 completed

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useDispatch } from "react-redux";
 import { addAnecdote } from "../reducers/anecdoteReducer";
 
@@ -13,12 +12,15 @@ const AnecdoteForm = () => {
   };
 
   return (
-    <form onSubmit={handleAddAnecdote}>
-      <div>
-        <input name="anecdote" />
-      </div>
-      <button>create</button>
-    </form>
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleAddAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
   );
 };
 
